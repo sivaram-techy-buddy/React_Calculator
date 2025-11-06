f6d0e79ff71f68196a6bc7134ef098a933580a80
Sivaram-avgira <sivaram@avgira.com>
2025-11-06 09:08:15 +0530

    context

--- calculator-app/src/App.tsx
+++ calculator-app/src/App.tsx
@@ -1,42 +1,44 @@
-import { useState } from 'react'
-import './App.css'
-import Button from './components/Button'
-import Display from './components/Display'
+import React from 'react';
+import './App.css';
+import { CalculatorProvider, useCalculator } from './Context/CalculatorContext';
+import Button from './components/Button';
+import Display from './components/Display';
 
+const Calculator: React.FC = () => {
+  const { input, setInput } = useCalculator();
 
-
-const App:React.FC =  () => {
-  const [count, setCount] = useState(0);
-  const [input, SetInput] = useState<string>('');
-
-
-  const handleButtonClick = (value: string) =>{
-    if(value === '='){
-        try {
-            SetInput(eval(input)); // Basic eval, can be replaced with safer parsing later
-        } catch(error) {
-         SetInput('Error');
-        }
-    } else if(value == 'C'){
-      SetInput("");
+  const handleButtonClick = (value: string) => {
+    if (value === '=') {
+      try {
+        setInput(eval(input));
+      } catch (error) {
+        setInput('Error');
+      }
+    } else if (value === 'C') {
+      setInput('');
     } else {
-      SetInput(input + value);
+      setInput(input + value);
     }
-  }
+  };
 
   return (
-    <>
-      <div className='calculator'>
-     <Display value = {input}/>
+    <div className="calculator">
+      <Display value={input} />
       <div className="buttons">
         {['7', '8', '9', '/', '4', '5', '6', '*', '1', '2', '3', '-', '0', '.', '=', '+', 'C'].map((value) => (
           <Button key={value} value={value} onClick={handleButtonClick} />
         ))}
       </div>
-      </div>
-      
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+const App: React.FC = () => {
+  return (
+    <CalculatorProvider>
+      <Calculator />
+    </CalculatorProvider>
+  );
+};
+
+export default App;
---
+++ calculator-app/src/Context/CalculatorContext.tsx
@@ -0,0 +1,29 @@
+import React, { createContext, useContext, useState, ReactNode } from 'react';
+
+interface CalculatorContextType {
+  input: string;
+  setInput: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const CalculatorContext = createContext<CalculatorContextType | undefined>(undefined);
+
+export const useCalculator = () => {
+  const context = useContext(CalculatorContext);
+  if (!context) {
+    throw new Error('useCalculator must be used within a CalculatorProvider');
+  }
+  return context;
+};
+
+interface CalculatorProviderProps {
+  children: ReactNode;
+}
+export const CalculatorProvider: React.FC<CalculatorProviderProps> = ({ children }) => {
+  const [input, setInput] = useState<string>('');
+
+  return (
+    <CalculatorContext.Provider value={{ input, setInput }}>
+      {children}
+    </CalculatorContext.Provider>
+  );
+};
